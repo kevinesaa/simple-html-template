ebabc333c8601c84f262cce47aa90dada44c3937
Kevin Esaa <kevin.esaa@gmail.com>
2022-10-15 19:21:42 -0400

    feature: trim file left

--- main-buildTasks.py
+++ main-buildTasks.py
@@ -1,7 +1,10 @@
 import os
 from buildTasks.cp.cp import cp
+from buildTasks.trimFileLeft.trimFileLeft import trimFileLeft
 
 SOURCE_DIR = os.path.abspath("src/")
 DIST_DIR = os.path.abspath("dist/")
 if __name__ == "__main__":
     cp(SOURCE_DIR, DIST_DIR)
+    trimFileLeft(DIST_DIR)
+#end
