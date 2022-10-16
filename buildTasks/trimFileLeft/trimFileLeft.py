453b3dc22bfa21bdaef34b5e097f9133b7ef2c35
Kevin Esaa <kevin.esaa@gmail.com>
2022-10-15 20:09:58 -0400

    feature: check file type before trim

--- buildTasks/trimFileLeft/trimFileLeft.py
+++ buildTasks/trimFileLeft/trimFileLeft.py
@@ -1,13 +1,13 @@
 import sys
 import os
-import shutil
+import mimetypes
 
 _BLANK = " "
 _TAB = "\t"
 _BREK_LINE = "\n"
 _CARRY_RETURN = "\r"
 
-def trimFileLeft(file):
+def trimFileLeft(file, ignoreExtension = []):
     
     file = os.path.abspath(os.path.normpath(file))
     if (not os.path.exists(file)):
@@ -18,13 +18,15 @@ def trimFileLeft(file):
         sfiles = os.listdir(file)
         for f in sfiles:
             filePath = os.path.join(file,f)
-            trimFileLeft(filePath)
+            trimFileLeft(filePath,ignoreExtension)
         #end
     #end
-    
-    if(os.path.isfile(file)):
+    type = mimetypes.guess_type(file)
+    if(os.path.isfile(file) and type[0] and type[0].startswith("text")):
+        
         bufferOut = []
         buffer = open(file, "r")
+
         for line in buffer:
             index = 0
             while(line.startswith(_BLANK,index) or 
