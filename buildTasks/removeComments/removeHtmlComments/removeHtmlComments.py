0776299f82fe3395d44e9c73b7ff427a882f22b5
GitArya27 <singharya2792@gmail.com>
2022-10-20 05:09:42 +0530

    Solves #34

--- buildTasks/removeComments/removeHtmlComments/removeHtmlComments.py
+++ buildTasks/removeComments/removeHtmlComments/removeHtmlComments.py
@@ -11,6 +11,36 @@ def removeHtmlComments(file):
     if(os.path.isdir(file)):
         raise Exception("Can't operate over a directory: "+file)
     #end
+    lines=[]
+    with open(file) as f:
+        lines = [line for line in f]
+        # print(lines)
+    flag =0
+    out_lines=[]
+    for i in lines:
+        if(i.find('<!--')!=-1 ):
+            comment_start = i.find('<!--')
+            comment_end   = i.find('-->')
+            if comment_end == -1 :
+                flag =1
+                out_lines.append(i[:comment_start]+'\n')
+            else:
+                j = i[:comment_start] + i[comment_end+3:]
+                out_lines.append(j)
+        elif flag:
+            comment_end   = i.find('-->')
+            if(comment_end!=-1):
+                j=i[comment_end+4:]
+                out_lines.append(j)
+                flag=0
+        else:
+            out_lines.append(i)
+    # print(out_lines)
+
+    with open(file, "w") as fp:
+        fp.writelines(out_lines)
+    
+
 #end
 
 if __name__ == "__main__":
